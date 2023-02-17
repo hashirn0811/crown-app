17f240e9c1c7abd1603b08381442d7e2c6349595
listless_#rn <105742488+hashirn0811@users.noreply.github.com>
2023-02-17 21:24:53 +0530

    switch to new react router api

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import './App.scss'
-import { Routes, Route } from 'react-router-dom'
-import Home from './routes/Home/'
-import Navigation from './components/Navigation/';
+import "./App.scss"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Home from "./routes/Home/"
+import Navigation from "./components/Navigation/"
+import Signin from "./routes/signin"
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigation />,
+    children: [
+      {
+        element: <Home />,
+        index: true,
+      },
+    ],
+  },
+  {
+    path: "login",
+    element: <Signin />,
+  },
+])
 
 function App() {
-  return (
-    <Routes>
-      <Route path='/' element={<Navigation />} >
-        <Route index element={<Home />} />
-      </Route>
-    </Routes> 
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
